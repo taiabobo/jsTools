1d716766b9c3bd30ec7a7a6082a28a55cfa48547
3Sharp <156508690@qq.com>
2015-11-10 14:54:18 +0800

    小型库
    增加dom绑定事件兼容性处理

---
+++ jsdk.js
@@ -0,0 +1,12 @@
+﻿//外观模式实现添加事件兼容方式
+function addEvent(dom, type, fn) {
+    if (dom.addEventListener) {
+        dom.addEventListener(type, fn, false);
+    }
+    else if (dom.attachEvent) {
+        dom.attachEvent('on' + type, fn);
+    }
+    else {
+        dom['on' + type] = fn;
+    }
+}

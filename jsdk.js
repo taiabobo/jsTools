fb9e04760c809d0dbd6d1dff3f4cc86f74305593
3Sharp <156508690@qq.com>
2015-11-18 16:27:05 +0800

    小型库
    创建xhr兼容

--- jsdk.js
+++ jsdk.js
@@ -29,3 +29,26 @@ var preventDefault = function (event) {
         event.returnValue = false;
     }
 }
+
+//创建XMLHttpRequest对象，兼容低版本浏览器;
+var createXHR = function () {
+    if (typeof XMLHttpRequest != "undefined") {
+        return new XMLHttpRequest();
+    }
+    else if (typeof ActiveXObject != "undefined") {
+        if (typeof arguments.callee.activeXString != "string") {
+            var versions = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0", "MSXML2.XMLHttp"], i, len;
+            for (i = 0, len = versions.length; i < len; i++) {
+                try {
+                    new ActiveXObject(versions[i]);
+                    arguments.callee.activeXString = versions[i];
+                    break;
+                } catch (ex) {
+                    //跳过
+                }
+            }
+        } else {
+            throw new Error("创建XMLHttpRequest失败。");
+        }
+    }
+}
